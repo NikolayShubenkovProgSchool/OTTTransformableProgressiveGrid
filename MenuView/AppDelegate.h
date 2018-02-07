29f49969c553766a64e7ed2ac8bf48d1c555c0bf
n.shubenkov <vokenbush@gmail.com>
2018-02-07 22:46:22 +0300

    Initial Commit

---
+++ MenuView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MenuView
+//
+//  Created by n.shubenkov on 07/02/2018.
+//  Copyright © 2018 n.shubenkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MenuView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MenuView
+//
+//  Created by n.shubenkov on 07/02/2018.
+//  Copyright © 2018 n.shubenkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
